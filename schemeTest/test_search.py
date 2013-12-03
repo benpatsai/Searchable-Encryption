a83bfe6baf08ea6592458282372b570071252303
Po-An Tsai <benpatsai@gmail.com>
2013-12-03 00:08:41 -0500

    fix test_search

--- schemeTest/test_search.py
+++ schemeTest/test_search.py
@@ -4,12 +4,12 @@ import client
 
 user = 'user1'
 f = open('12345', 'w')
-f.write('1 2 3 4 5')
+f.write('1 2 3 4 5 \n')
 f = open('2468', 'w')
-f.write('2 4 6 8')
+f.write('2 4 6 8 \n')
 f = open('13579', 'w')
-f.write('1 3 5 7 9')
-
+f.write('1 3 5 7 9\n')
+f.close()
 
 #client.upload(user, path, filename)
 client.upload(user, '12345', 'test_12345')
