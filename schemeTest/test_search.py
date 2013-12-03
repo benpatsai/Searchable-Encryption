f2317dbedb7896ce652bfd72ad7b08279c609888
Po-An Tsai <benpatsai@gmail.com>
2013-12-03 00:00:51 -0500

    automatically make file in test_search

--- schemeTest/test_search.py
+++ schemeTest/test_search.py
@@ -3,7 +3,15 @@
 import client 
 
 user = 'user1'
-# client.upload(user, path, filename)
+f = open('12345', 'w')
+f.write('1 2 3 4 5')
+f = open('2468', 'w')
+f.write('2 4 6 8')
+f = open('13579', 'w')
+f.write('1 3 5 7 9')
+
+
+#client.upload(user, path, filename)
 client.upload(user, '12345', 'test_12345')
 client.upload(user, '2468', 'test_2468')
 client.upload(user, '13579', 'test_13579')
